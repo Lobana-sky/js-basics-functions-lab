a13b21903fe79e613d4574d69722ef913fd1bc5e
mmelihdogan <doganmmelih@gmail.com>
2020-01-23 19:58:18 +0300

    finished the assignments

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // Code your solution in this file!
+
 function distanceFromHqInBlocks(block) {
-    if (block > 42) {
-        return block - 42;
-    } else {
-        return 42 - block;
-    }
+    return Math.abs(block - 42);
 }
 
 function distanceFromHqInFeet(block) {
     // each block = 264 feet
     return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(from, to) {
+    return Math.abs(from - to) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet < 400) {
+        return 0;
+    } else if (feet < 2000) {
+        feet -= 400; 
+        return 0.02 * feet;
+    } else if (feet > 2500) {
+        return "cannot travel that far";
+    } 
+    return 25;
 }
