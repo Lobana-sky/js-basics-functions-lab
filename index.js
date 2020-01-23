97c556024bdf4b5876d49f8fc4c5215d87932356
mmelihdogan <doganmmelih@gmail.com>
2020-01-23 19:35:50 +0300

    calculating distancesfromhq in blocks and feet

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block > 42) {
+        return block - 42;
+    } else {
+        return 42 - block;
+    }
+}
+
+function distanceFromHqInFeet(block) {
+    // each block = 264 feet
+    return distanceFromHqInBlocks(block) * 264;
+}
